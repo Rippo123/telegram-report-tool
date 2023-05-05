92def4c4ae07a3e12417bae4f293c4ca18177469
Rippo123 <98217663+Rippo123@users.noreply.github.com>
2023-05-05 18:49:28 +0530

    Update report.py

--- report.py
+++ report.py
@@ -2,8 +2,8 @@ import telegram
 import csv
 
 # Replace the following with your own Telegram API key and channel name
-api_key = 'YOUR_API_KEY'
-channel_name = 'YOUR_CHANNEL_NAME'
+api_key = '6260bf74ef8f728839d91095d0522101'
+channel_name = 'status_group_malayalam'
 
 # Initialize the Telegram bot using the API key
 bot = telegram.Bot(api_key)
